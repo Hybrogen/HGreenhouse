44d87030e49c12682410a8b0b0a42bf75a37097f
Hybrogen <hon@zinchon.com>
2022-03-31 14:52:24 +0800

    修改 POST 接口的获取还有小 bug

--- HGreenhouse/views.py
+++ HGreenhouse/views.py
@@ -15,50 +15,55 @@ def index(request):
 
 def set_hconfig(change_info: dict) -> bool:
     with open('HModules/thresholds', encoding='utf8') as f: hconfig = json.loads(f.readline())
-    for k, v in change_info:
+    for k, v in change_info.items():
         if k in hconfig: hconfig[k] = v
-    with open('HModules/thresholds_reset', encoding='utf8') as f: f.write(json.dumps(hconfig))
+    with open('HModules/thresholds_reset', 'w', encoding='utf8') as f: f.write(json.dumps(hconfig))
     return True
 
 def set_temperature(request):
+    query_data = json.loads(request.body)
     rdata = dict()
-    rdata['temperature'] = float(request.POST['num'][0])
+    rdata['temperature'] = float(query_data['num'])
     set_hconfig(rdata)
     rdata['state'] = 'ok'
     return JsonResponse(rdata)
 
 def set_humidity(request):
+    query_data = json.loads(request.body)
     rdata = dict()
-    rdata['humidity'] = int(request.POST['num'][0])
+    rdata['humidity'] = int(query_data['num'])
     set_hconfig(rdata)
     rdata['state'] = 'ok'
     return JsonResponse(rdata)
 
 def set_light(request):
+    query_data = json.loads(request.body)
     rdata = dict()
-    rdata['light'] = int(request.POST['num'][0])
+    rdata['light'] = int(query_data['num'])
     set_hconfig(rdata)
     rdata['state'] = 'ok'
     return JsonResponse(rdata)
 
 def set_water(request):
+    query_data = json.loads(request.body)
     rdata = dict()
     rdata['water_auto'] = False
-    rdata['water_state'] = bool(request.POST['status'][0])
+    rdata['water_state'] = bool(query_data['status'])
     set_hconfig(rdata)
     rdata['state'] = 'ok'
     return JsonResponse(rdata)
 
 def set_curtain(request):
+    query_data = json.loads(request.body)
     rdata = dict()
     rdata['curtain_auto'] = False
-    rdata['curtain_state'] = bool(request.POST['status'][0])
+    rdata['curtain_state'] = bool(query_data['status'])
     set_hconfig(rdata)
     rdata['state'] = 'ok'
     return JsonResponse(rdata)
 
 def get_data(request):
-    print(f"get_data = {request.GET}")
+    # print(f"get_data = {request.GET}")
     rdata = dict()
     rdata['pid'] = request.GET['houseNum'][0]
     if 'startTime' in request.GET and 'endTime' in request.GET:
